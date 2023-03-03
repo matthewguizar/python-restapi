042dacbc4afed609899aeda3f063cd7ea3db61f4
Matthew Guizar <matthewrguizar@gmail.com>
2023-03-03 10:32:22 -0800

    implementing auto reloading for docker container

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ def get_stores():
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"] == store["name"]:
+            abort(400, message=f"store already exists")
     store_id = uuid.uuid4().hex
     store = {**store_data, "id": store_id}  # unpacking **
     stores[store_id] = store
@@ -22,10 +30,24 @@ def create_store():
 
 
 @app.post("/item")
-def create_item(name):
+def create_item():
     item_data = request.get_json()
-    if item_data["store_id"] not in stores:
-        return abort(404, message="store not found")
+
+    if (
+            "price" not in item_data
+            or "store_id" not in item_data
+            or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload "
+        )
+    for item in items.values():
+        if (
+                item_data["name"] == item["name"]
+                and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message=f"item already exists")
 
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -51,4 +73,37 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return abort(404, message= "item not found")
+        return abort(404, message="item not found")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="bad request. ensure 'price' and 'name' are included in the JSON payload")
+
+    try:
+        item = items[item_id]
+        item |= item_data  # merges two dictionaries
+
+        return item
+    except KeyError:
+        abort(404, message="item not found")
+
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="item not found")
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "store deleted"}
+    except KeyError:
+        abort(404, message="store not found")
